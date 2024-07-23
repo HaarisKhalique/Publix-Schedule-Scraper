e10f30bbc1f18610cdd711c139efaa230e7268fe
Haaris Khalique <ha145268@ucf.edu>
2024-07-23 00:23:06 -0400

    Commented code and created a changelog

--- pubscraper.py
+++ pubscraper.py
@@ -1,3 +1,19 @@
+'''
+Publix Schedule Scraper by Haaris Khalique
+
+This script will automate the login process to Publix PASSPort, 
+retrieve your schedule information, and copy it into your preferred 
+calendar app.
+
+'''
+'''
+TO DO LIST (1 = COMPLETE, 0 = INCOMPLETE)
+
+1 - Automate login process
+0 - Scrape schedule information
+0 - Copy data into Apple Calendar
+'''
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
@@ -5,33 +21,36 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from getpass import getpass
 
-#options to prevent browser from closing automatically during testing
+#Options to prevent browser from closing automatically during testing
 options = webdriver.EdgeOptions()
 options.add_experimental_option("detach",True)
 driver = webdriver.Edge(options = options)
 
-WEBSITE = "https://www.publix.org/passport/scheduling/schedule"
-LOGIN_BUTTON = (By.XPATH, "/html/body/main/div[3]/div/div/form/button")
-NEXT_BUTTON = (By.ID, "idSIButton9")
+#Constants
+WEBSITE = "https://www.publix.org/passport/scheduling/schedule" #URL to access schedule page
+LOGIN_BUTTON = (By.XPATH, "/html/body/main/div[3]/div/div/form/button") #Locate login button to begin login process
+NEXT_BUTTON = (By.ID, "idSIButton9") #Locate "next" button to proceed after entering username and password.
+EMAIL_FIELD = (By.ID, "i0116") #Locate username field
+PASSWORD_FIELD = (By.ID, "i0118") #Locate password field
+AUTH_BUTTON = (By.XPATH, '//*[@id="idDiv_SAOTCS_Proofs"]/div[1]/div') #Locate MS Authenticator button to complete login
 
-EMAIL_FIELD = (By.ID, "i0116")
-PASSWORD_FIELD = (By.ID, "i0118")
-
-AUTH_BUTTON = (By.XPATH, '//*[@id="idDiv_SAOTCS_Proofs"]/div[1]/div')
 
 def main():
-    #prompt user for credentials
+    #Prompt user for credentials on command line prior to automated login
     username = input("Enter your Publix PASSPort username: ")
     password = getpass("Enter your Publix PASSPort password: ")
 
     try:
         driver.get(WEBSITE)
 
+        #Wait until login button is clickable and click to begin login process
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(LOGIN_BUTTON)).click()
 
+        #Wait until username field is present, populate the field, and click "next" button to proceed to password entry
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(EMAIL_FIELD)).send_keys(username)
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(NEXT_BUTTON)).click()
 
+        #Wait until password field is present, populate the field, and click next (now labeled "sign in" in browser) button to proceed to Two-Factor Authentication
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(PASSWORD_FIELD)).send_keys(password)
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(NEXT_BUTTON)).click()
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(AUTH_BUTTON)).click()
@@ -39,6 +58,8 @@ def main():
     except Exception as e:
         print("An error occurred")
 
+
+    #Halt after completion
     input("All done! Press enter to close the browser...")
     driver.quit()
 
